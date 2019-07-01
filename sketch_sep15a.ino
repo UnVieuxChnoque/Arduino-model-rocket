d1dd51435de4e6600758779446410e185dca190c
UnVieuxChnoque <52420514+UnVieuxChnoque@users.noreply.github.com>
2019-07-01 19:10:46 +0300

    Update sketch_sep15a.ino

--- sketch_sep15a.ino
+++ sketch_sep15a.ino
@@ -1,49 +1,62 @@
-#include <dht11.h> // charger la librairie
-#include <EEPROM.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <SPI.h>
+#include <SD.h>
 
-dht11 DHT11; // Créer le capteur
+MPU6050 capteur(0x68);
+int16_t ax, ay, az,gx, gy, gz;
+int16_t nbrMesures = 0;
+int32_t t =0;
+float moyenneAx,moyenneAy,moyenneAz,moyenneGx,moyenneGy,moyenneGz;
 
-#define DHT11PIN 3 // Définir la pin utilisée
+File _42_;
 
-int addresse = 0;
-void setup()
-{
-  // Initialiser la liaison série
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
+  capteur.initialize();
+  delay(5000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(900000);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  t=millis();
+  while(millis()-t<60000){
+    capteur.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    //Serial.println("Stabilisation... "+String(millis()-t/300)+"%");
+  }
+  
+  t=millis();
+  while(millis()-t<10000){
+      //Serial.println("Étalonnage... "+String((millis()-t)/100)+"%");
+      capteur.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      moyenneAx+=ax;
+      moyenneAy+=ay;
+      moyenneAz+=az;
+      moyenneGx+=gx;
+      moyenneGy+=gy;
+      moyenneGz+=gz;;
+      nbrMesures++;
+    }
+    moyenneAx/=nbrMesures;
+    moyenneAy/=nbrMesures;
+    moyenneAy-=16384;
+    moyenneAz/=nbrMesures;
+    moyenneGx/=nbrMesures;
+    moyenneGy/=nbrMesures;
+    moyenneGz/=nbrMesures;
+    //Serial.println(String(moyenneAx)+"\t"+String(moyenneAy)+"\t"+String(moyenneAz)+"\t"+String(moyenneGx)+"\t"+String(moyenneGy)+"\t"+String(moyenneGz));
+    SD.begin(4);
+    SD.remove("42.txt");
 }
 
-void loop()
-{
-  Serial.print(" --- Nouvelle lecture --- ");
-
-  int chk = DHT11.read(DHT11PIN); // Lecture du capteur
-
-  Serial.print("Etat du capteur: ");
-
-  switch (chk) // Est-ce que le capteur fonctionne?
-  {
-    case DHTLIB_OK: 
-                Serial.println("OK"); 
-                break;
-    case DHTLIB_ERROR_CHECKSUM: 
-                Serial.println("Checksum erreur"); 
-                break;
-    case DHTLIB_ERROR_TIMEOUT: 
-                Serial.println("Time out erreur"); 
-                break;
-    default: 
-                Serial.println("Erreur inconnue"); 
-                break;
+void loop(){
+  _42_=SD.open("42.txt",FILE_WRITE);
+  while(millis()%100!=0){
   }
-
-  Serial.print("humidite: "); // Humidité
-  Serial.println((float)DHT11.humidity, 3);
-
-  Serial.print("Temperature: "); // Température
-  Serial.println((float)DHT11.temperature, 3);
-
-  delay(1000); // Pause avant prochaine lecture
-
-  addresse++
-
+  capteur.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  _42_.println(String(ax-moyenneAx)+"\t"+String(ay-moyenneAy)+"\t"+String(az-moyenneAz)+"\t"+String(gx-moyenneGx)+"\t"+String(gy-moyenneGy)+"\t"+String(gz-moyenneGz));
+  //Serial.println(String(ax-moyenneAx)+"\t"+String(ay-moyenneAy)+"\t"+String(az-moyenneAz)+"\t"+String(gx-moyenneGx)+"\t"+String(gy-moyenneGy)+"\t"+String(gz-moyenneGz));
+  _42_.close();
 }
